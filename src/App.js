c5d6228054b1e43036fd5fb55948b70fcd52d721
Aoki-gif <63574609+Aoki-gif@users.noreply.github.com>
2022-07-11 14:36:00 +0900

    テスト 画面遷移 Jsonの呼び出し

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import "./styles.css";
+import Routers from "./Routers";
+import { BrowserRouter } from "react-router-dom";
 
-export default function App() {
+const App = () => {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Routers />
     </div>
   );
-}
+};
+
+export default App;
